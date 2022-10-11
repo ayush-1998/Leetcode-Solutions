a92feab7e3d636a126179849c3afc8d6031bbcaa
Ayush Pegwar <64705130+ayush-1998@users.noreply.github.com>
2022-10-11 19:07:57 +0530

    Time: 5 ms (36.70%), Space: 54.4 MB (75.15%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,27 +1,43 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int[] arr = new int[nums.length];
-        int itr = nums.length-1;
-        for(int i=0;i<nums.length;i++)
+//         int[] arr = new int[nums.length];
+//         int itr = nums.length-1;
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             if(nums[i] == 0)
+//             {
+//                 arr[itr--] = nums[i];
+//             }
+//         }
+        
+//         itr = 0;
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             if(nums[i] != 0)
+//             {
+//                 arr[itr++] = nums[i];
+//             }
+//         }
+        
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             nums[i] = arr[i];
+//         }
+        if(nums == null || nums.length == 0) return; 
+        
+        int itr = 0;
+        for(int num : nums)
         {
-            if(nums[i] == 0)
+            if(num != 0)
             {
-                arr[itr--] = nums[i];
+                nums[itr++] = num;
             }
         }
         
-        itr = 0;
-        for(int i=0;i<nums.length;i++)
+        while(itr < nums.length)
         {
-            if(nums[i] != 0)
-            {
-                arr[itr++] = nums[i];
-            }
+            nums[itr++] = 0;
         }
         
-        for(int i=0;i<nums.length;i++)
-        {
-            nums[i] = arr[i];
-        }
     }
 }
