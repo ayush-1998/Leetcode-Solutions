54a112ccbaab0be3e2ba00d78cdaee964aab2fba
Ayush Pegwar <64705130+ayush-1998@users.noreply.github.com>
2022-10-14 08:50:46 +0530

    Time: 2 ms (91.23%), Space: 42.3 MB (21.24%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean isValid(String str) {
+         Stack<Character> st = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } else if (ch == ')') {
+                if (st.size() == 0 || st.peek() != '(') {
+                    
+                    return false;
+                } else {
+                    st.pop();
+                }
+            } else if (ch == '}') {
+                if (st.size() == 0 || st.peek() != '{') {
+                    
+                    return false;
+                } else {
+                    st.pop();
+                }
+            } else if (ch == ']') {
+                if (st.size() == 0 || st.peek() != '[') {
+                
+                    return false;
+                } else {
+                    st.pop();
+                }
+            } else {
+                // nothing
+            }
+        }
+
+        if (st.size() == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
