a5998178e4cbef23c393abd1f89846bd79787769
Ayush Pegwar <64705130+ayush-1998@users.noreply.github.com>
2022-10-31 08:53:37 +0530

    Time: 3 ms (9.45%), Space: 45.8 MB (54.67%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.java
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.java
@@ -1,18 +1,36 @@
 class Solution {
+
+    // public boolean isToeplitzMatrix(int[][] matrix) {
+    //     for(int i=1;i<matrix.length;i++)
+    //     {
+    //         for(int j=1;j<matrix[0].length;j++)
+    //         {
+    //             if(matrix[i][j] == matrix[i-1][j-1])
+    //             {
+    //                 continue;
+    //             }
+    //             else
+    //             {
+    //                 return false;
+    //             }
+    //         }
+    //     }
+    //     return true;
+    // }
+
+    //For follow up 1
     public boolean isToeplitzMatrix(int[][] matrix) {
-        for(int i=1;i<matrix.length;i++)
-        {
-            for(int j=1;j<matrix[0].length;j++)
-            {
-                if(matrix[i][j] == matrix[i-1][j-1])
-                {
-                    continue;
-                }
-                else
-                {
-                    return false;
-                }
+        if (matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0) return true;
+        int row = matrix.length;
+        int col = matrix[0].length;
+        List<Integer> buffer = new LinkedList<>();
+        for (int j = 0; j < col; j++) buffer.add(matrix[0][j]);
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < col; j++) {
+                if (buffer.get(j - 1) != matrix[i][j]) return false;
             }
+            buffer.remove(buffer.size() - 1);
+            buffer.add(0, matrix[i][0]);
         }
         return true;
     }
