bb0eef31d6845126f0a118d9b76fe3f3bc5a6e3c
Ayush Pegwar <64705130+ayush-1998@users.noreply.github.com>
2022-11-10 08:26:07 +0530

    Time: 619 ms (23.54%), Space: 360.5 MB (21.91%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -1,13 +1,33 @@
 class Solution {
     public String removeDuplicates(String s) {
-          StringBuilder sb = new StringBuilder();
-        for (char ch: s.toCharArray()) {
-            if ( sb.length() != 0 && sb.charAt(sb.length() - 1) == ch ) {
-                sb.deleteCharAt(sb.length() - 1);
-            } else {
-                sb.append(ch);
-            }
+    //       StringBuilder sb = new StringBuilder();
+    //     for (char ch: s.toCharArray()) {
+    //         if ( sb.length() != 0 && sb.charAt(sb.length() - 1) == ch ) {
+    //             sb.deleteCharAt(sb.length() - 1);
+    //         } else {
+    //             sb.append(ch);
+    //         }
+    //     }
+    //     return sb.toString();
+    // }
+        
+          Stack<Character> stack = new Stack<>();
+    stack.push(s.charAt(0));
+    for(int i=1;i<s.length();i++){
+        char c = s.charAt(i);
+		// if stack is empty or the top element of the stack is not current we have then push into the stack
+        if(stack.isEmpty() || stack.peek() != c){
+            stack.push(c);
+        }// otherwise pop from the stack
+		else{
+            stack.pop();
         }
-        return sb.toString();
     }
+	// pop all the element which is left in the stack and store into the string and return;
+    String ans="";
+    while (!stack.isEmpty()){
+        ans=stack.pop()+ans;
+    }
+    return ans;
+}
 }
