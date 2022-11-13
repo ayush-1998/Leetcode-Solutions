161e0a4a1abd03ee3aa67cd208206d3c24c4fd22
Ayush Pegwar <64705130+ayush-1998@users.noreply.github.com>
2022-11-13 19:29:39 +0530

    Time: 6 ms (90.49%), Space: 43.2 MB (71.70%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -0,0 +1,63 @@
+class Solution {
+    public String reverseWords(String s) {
+        if(s == null) return null;
+        char[] a = s.toCharArray();
+        int n = a.length;
+        
+        reverse(a, 0, n-1);
+        
+        reverseWords(a, n);
+        
+        return  cleanSpace(a, n);
+    }
+    
+    public void reverse(char[] a, int i, int j)
+    {
+        while(i < j)
+        {
+            char temp = a[i];
+            a[i++] = a[j];
+            a[j--] = temp;
+        }
+    }
+    
+    public void reverseWords(char[] a, int n)
+    {
+        int i=0, j=0;
+        while(i < n)
+        {
+            while(i < j || i < n && a[i] == ' ')
+            {
+                i++;
+            }
+            
+            while(j < i || j < n && a[j] != ' ')
+            {
+                j++;
+            }
+            
+            reverse(a, i, j-1);
+        }
+    }
+    
+    public String cleanSpace(char[] a, int n)
+    {
+        int i=0, j=0;
+        while(j < n)
+        {
+            while(j < n && a[j] == ' ') j++;
+            while(j < n && a[j] != ' ')
+            {
+                a[i++] = a[j++];
+            }
+            while(j < n && a[j] == ' ') j++;
+            if(j < n)
+            {
+                a[i++] = ' ';
+            }
+            
+        
+        }
+        return new String(a).substring(0,i);
+    }
+}
