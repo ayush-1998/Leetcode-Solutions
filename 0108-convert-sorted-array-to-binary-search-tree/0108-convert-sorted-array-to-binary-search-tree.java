cd8084a17ddc86b3154a954cce3f4ffa49bab57b
Ayush Pegwar <64705130+ayush-1998@users.noreply.github.com>
2022-11-19 11:07:53 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (91.45%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.java
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.java
@@ -19,15 +19,15 @@ class Solution {
         return construct(nums,0,n-1);
         
     }
+
     public TreeNode construct(int[] nums, int si, int ei)
     {
+        if(si > ei) return null;
         
-        if(si > ei)
-            return null;
         int mid = (si + ei)/2;
         TreeNode node = new TreeNode(nums[mid]);
         
-        node.left = construct(nums,si, mid-1);
+        node.left = construct(nums,si,mid-1);
         node.right = construct(nums,mid+1,ei);
         
         return node;
