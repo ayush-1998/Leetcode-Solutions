035512b8a955c1c6086905eeb2dd1b2179008407
Ayush Pegwar <64705130+ayush-1998@users.noreply.github.com>
2022-11-23 16:13:40 +0530

    Time: 5 ms (95.64%), Space: 42.4 MB (96.15%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,17 +1,24 @@
 class Solution {
     public int firstUniqChar(String s) {
-        if (s.isEmpty())
-			return -1;
-
-		int[] count = new int[26];
-		for (int i = s.length() - 1; i >= 0; i--) {
-			count[s.charAt(i) - 'a']++;
-		}
-		for (int i = 0; i < s.length(); i++) {
-			if(count[s.charAt(i) - 'a'] == 1) {
-				return i;
-			}
-		}
-		return -1;
+        
+        if(s.length() == 0) return -1;
+        
+        int[] count = new int[26];
+        for(int i=s.length()-1;i>=0;i--)
+        {
+            char ch = s.charAt(i);
+            count[ch-'a']++;
+        }
+        
+        for(int i=0;i<s.length();i++)
+        {
+            char ch = s.charAt(i);
+            if(count[ch-'a'] == 1)
+            {
+                return i;
+            }
+        }
+        
+        return -1;
     }
 }
