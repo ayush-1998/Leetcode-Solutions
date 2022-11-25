1c5ab14c2a8a433da5f8181847d7976b8cfbf5de
Ayush Pegwar <64705130+ayush-1998@users.noreply.github.com>
2022-11-25 10:32:53 +0530

    Time: 1 ms (100.00%), Space: 43.2 MB (88.12%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+      int count = 0; // created a count variable
+        
+        for(int i = 0; i < flowerbed.length; i++){ // start looping from 1st position till end of the array
+            if(flowerbed[i] == 0){ // only if flower plant place is empty we will gonna perform this logic
+                // creating two values previous & next. Are they 0 or 1
+                int prev = (i == 0 || flowerbed[i - 1] == 0) ? 0 : 1;
+                int next = (i == flowerbed.length - 1 || flowerbed[i + 1] == 0) ? 0 : 1;
+                
+                if(prev == 0 && next == 0){ // and only if these two values are 0
+                    flowerbed[i] = 1; // will plant a flower
+                    count++; // increment the count
+                }
+            }
+        }
+        return count >= n; // in the end we just need to check is count we get is greater or equals to n
+    }
+}
